aab340fac578d5c6ba66d89bc0e9c84e31ebfdb4
vlnhz <balvalentino2@gmail.com>
2023-02-09 11:04:57 -0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,12 @@
+import random
+
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile, 2):
+        if random.randrange(num):
+            continue
+        line = aline
+    return line
+
+archivo = open('frases.txt')
+print(random_line(archivo))
