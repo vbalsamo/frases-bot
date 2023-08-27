f4b459d4d6509f1c648f3c612aa0e5cf885fc0f1
Valentino Balsamo <balvalentino@gmail.com>
2023-08-27 14:11:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,11 @@ import os
 import tweepy
 from frases import frases
 
-consumerKey = os.environ['API_KEY']
-consumerSecret = os.environ['API_KEY_SECRET']
-accessKey = os.environ['ACCESS_TOKEN']
-accessSecret = os.environ['ACCESS_TOKEN_SECRET']
+client = tweepy.Client(
+    consumer_key=os.environ['API_KEY'],
+    consumer_secret=os.environ['API_KEY_SECRET'],
+    access_token=os.environ['ACCESS_TOKEN'],
+    access_token_secret=os.environ['ACCESS_TOKEN_SECRET']
+)
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessKey, accessSecret)
-api = tweepy.API(auth)
-
-api.update_status(status = random.choice(frases))
+client.create_tweet(text=random.choice(frases))
